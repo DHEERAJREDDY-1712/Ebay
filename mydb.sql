af3510df83e2333b879a0cd38139ac2bdcb7d5d2
DHEERAJREDDY-1712 <66180599+DHEERAJREDDY-1712@users.noreply.github.com>
2020-11-29 18:34:39 -0500

    Update mydb.sql

--- mydb.sql
+++ mydb.sql
@@ -1,11 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.0.3
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Nov 29, 2020 at 04:56 PM
--- Server version: 10.4.14-MariaDB
--- PHP Version: 7.2.34
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
